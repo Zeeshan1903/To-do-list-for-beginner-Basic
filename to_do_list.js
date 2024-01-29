371e30bdd53e34d09f1328ad3be873796fb6326c
Zeeshan1903 <156703447+Zeeshan1903@users.noreply.github.com>
2024-01-29 16:24:50 +0530

    Update to_do_list.js

--- to_do_list.js
+++ to_do_list.js
@@ -10,20 +10,27 @@ function create() {
     let date = new Date();
 
     let content = input.value;
-    console.log(content);
 
+
+    //Setting the item in the local storage 
     localStorage.setItem('todo', JSON.stringify([content]));
 
     let show = document.getElementById('p');
 
+    //Making a texttag element which will contain the date,delete option and our text node 
+    //texttag is an element wiht textNode,date, delete as its children 
     let texttag = document.createElement('p');
     let textNode = document.createTextNode('hi');
     texttag.setAttribute('id', 'list_itmes');
 
+    //Giving the value from the local storage to our textNode
     textNode.textContent = JSON.parse(localStorage.getItem('todo'));
 
+    //Adding the textNode to our TextTag parent class
     texttag.appendChild(textNode);
 
+
+    //Making a delete fn.
     let delete_btn = document.createElement('button');
     texttag.appendChild(delete_btn);
     delete_btn.setAttribute('id', 'delete');
@@ -31,30 +38,37 @@ function create() {
 
     let happens = false;
     delete_btn.addEventListener('click',del);
+
+    //adding addevet to delete fn 
     function del(){
         happens = true;
+
+        //this will clear the local storage memeory
         textNode.textContent = (localStorage.removeItem('todo'));
+        // this will clear the written text node and will remove all the child 
         texttag.innerHTML = ' ';
     }
 
     
-    
+
+
+    //Creating a  date node 
     let date_btn = document.createElement('p');
     texttag.appendChild(date_btn);
     date_btn.setAttribute('id', 'date');
     date_btn.innerHTML += (date.toDateString() + " " + date.getHours() + ":" + date.getMinutes());
 
-
+    //Adding the texttag node to the main Paragraph
     show.appendChild(texttag);
 
+    //Making the input clear after adding the text from input 
     input.value = ' ';
 }
 
 
-
-
-
+//Clear function 
 function clear() {
     let data = document.getElementById('p');
+    //this will delete the whole paragraph node not like delete fn single single texttag nodes 
     data.innerHTML = ' ';
 }
